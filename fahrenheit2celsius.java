0a850e4d9602f4de962350d16ebdb8701648dc62
jacob <cio_zhang@163.com>
2017-05-03 09:11:37 +0800

    add my first java program

---
+++ fahrenheit2celsius.java
@@ -0,0 +1,8 @@
+public class fahrenheit2celsius{
+	public static void main(String []args){
+		double fahrenheit=50;
+		double celsius;
+		celsius = (fahrenheit-32)*5/9;
+		System.out.println(celsius);
+	}
+}
