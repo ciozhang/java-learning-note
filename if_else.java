3b2ffba7dabb2562faf4857e06fb6c5799a519b6
jacob <cio_zhang@163.com>
2017-05-05 07:46:47 +0800

    add if_else

---
+++ if_else.java
@@ -0,0 +1,15 @@
+//Assume these can have any value:
+public class if_else{
+	public static void main(String[] args){
+		boolean isSnowing = false;
+		boolean isRaining = true;
+		double temperature = 60.0;
+		//TODO: print "Let’s stay home." if its raining, snowing or
+		//below 50 degrees and print "Let’s go out!" otherwise.
+		if (isRaining || isSnowing || temperature < 50){
+			System.out.println("Let's stay at home.");
+		} else{
+			System.out.println("Let's go out!");
+		}
+	}
+}
